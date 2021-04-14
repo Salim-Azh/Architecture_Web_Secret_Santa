da313d4b2e685361fe82afba74a9f7704e7d9f95
Salim_Azh <azharhoussen.salim@gmail.com>
2021-04-14 18:35:02 +0200

    style : comment removed

--- config/db.js
+++ config/db.js
@@ -29,7 +29,4 @@ connection.getConnection((err, conn) => {
     return
 })
 
-/*.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.log('Failed to connect to MongoDB', err))*/
-
 module.exports = connection;
